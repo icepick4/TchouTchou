2d39b22930a39b811387245c511c8d72e6a2c321
Qypol342 <Qypol342@gmail.com>
2022-09-24 22:00:49 +0200

    simple exemple session

---
+++ dev/session/index.js
@@ -0,0 +1,6 @@
+const h1 = document.querySelector("h1");
+
+
+if (sessionStorage.getItem("login") == "true"){h1.innerHTML="LOGIN";};
+
+
---
+++ dev/session/login.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector("button");
+
+
+btn.addEventListener('click', (event) => {sessionStorage.setItem("login","true");});
+
