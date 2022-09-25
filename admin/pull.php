6568c02b8188b6a8b208cf79a22657559126e2f9
Qypol342 <Qypol342@gmail.com>
2022-09-25 13:34:41 +0200

    add pull.php webhook

---
+++ admin/pull.php
@@ -0,0 +1,2 @@
+<?php shell_exec( 'cd /var/www/sae-sncf; git reset --hard origin/prod; git clean -d -f; git pull https://gitlab-ci-token:glpat-wFygfmCs9RddfneY2Zvo@forge.univ-lyon1.fr/p2103642/sae-sncf.git prod' ); ?>
+
