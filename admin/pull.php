fab806acdaccc49fec4d27777d2711d55346dd37
MAEGHT LOAN p2103642 <loan.maeght@etu.univ-lyon1.fr>
2022-10-31 15:32:03 +0000

    change from prod to main

--- admin/pull.php
+++ admin/pull.php
@@ -1,2 +1,2 @@
-<?php echo shell_exec( 'cd /var/www/sae-sncf; git reset --hard origin/prod; git clean -d -f; git pull https://gitlab-ci-token:glpat-wFygfmCs9RddfneY2Zvo@forge.univ-lyon1.fr/p2103642/sae-sncf.git prod'); ?>
+<?php echo shell_exec( 'cd /var/www/sae-sncf; git reset --hard origin/prod; git clean -d -f; git pull https://gitlab-ci-token:glpat-wFygfmCs9RddfneY2Zvo@forge.univ-lyon1.fr/p2103642/sae-sncf.git main'); ?>
 
