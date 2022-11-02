691d72491bc4cd781ad8b4faa34028a77e36c7b9
qypol342 <qypol342@gmail.com>
2022-11-02 11:55:14 +0100

    add accueil emp

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>SNCF</title>
-	<link rel="stylesheet" href="src/CSS/common.css">
+	<link rel="stylesheet" type="text/css" href="/src/CSS/common.css">
 </head>
 <body>
 hello
---
+++ src/PHP/accueil_employe.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>SNCF</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+	<link rel="stylesheet" type="text/css" href="/src/CSS/common.css">
+	<link rel="stylesheet" type="text/css" href="/src/CSS/AC_EMP.css">
+</head>
+<body>
+	<div class="tmp"></div>
+	<div class="tmp"></div>
+
+
+	<div class="main">
+		<article>
+			<img src="/src/ASSETS/work_il.svg">
+			<div class="arti">
+				<h2>Bienvenu</h2>
+				<p>Sed fruatur sane hoc solacio atque hanc insignem ignominiam, quoniam uni praeter se inusta sit, putet esse leviorem, dum modo, cuius exemplo se consolatur, eius exitum expectet, praesertim cum in Albucio nec Pisonis libidines nec audacia Gabini fuerit ac tamen hac una plaga conciderit, ignominia senatus.</p>
+			</div>
+		</article>
+		<article>
+			<img src="/src/ASSETS/station_il.svg">
+			<div class="arti">
+				<h2>Gestion des quais</h2>
+				<p>Sed fruatur sane hoc solacio atque hanc insignem ignominiam, quoniam uni praeter se inusta sit.</p>
+				<a href="">
+				<div class="btn"><p>accéder</p>
+					<img src="/src/ASSETS/arrow.svg"></div></a>
+			</div>
+		</article>
+		<article>
+			<img src="/src/ASSETS/engineer_il.svg">
+			<div class="arti">
+				<h2>Maintenance</h2>
+				<p>Sed fruatur sane hoc solacio atque hanc insignem ignominiam, quoniam uni praeter se inusta sit.</p>
+				<div class="btn"><p>accéder</p>
+					<img src="/src/ASSETS/arrow.svg"></div></a>
+			</div>
+		</article>
+	</div>
+
+	<div class="tmp"></div>
+
+</body>
+</html>
