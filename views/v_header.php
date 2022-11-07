276ce7bead2e05b497238d2b50eb9c122a591c70
VOILLOT ANTOINE p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2022-11-07 19:09:52 +0000

    Update v_header.php

--- views/v_header.php
+++ views/v_header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="Language" content="<?= LANG ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?= echo TITLE ?></title>
+    <title><?= TITLE ?></title>
     <link href="<?php echo PATH_CSS."common.css" ?>" rel="stylesheet" />
   </head>
   <body>
