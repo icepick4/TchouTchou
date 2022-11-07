fb4bd5bd6a6d50b57bd8fe55cbebfdb6b3859b59
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2022-11-07 13:33:49 +0100

    Add header files

---
+++ languages/FR-fr.php
@@ -0,0 +1,8 @@
+<?php
+
+define('TITLE','Tchoutchou');
+define('TITLE_HOME','Accueil');
+define('TITLE_PURCHASE','Achats');
+
+
+?>
--- views/v_header.php
+++ views/v_header.php
@@ -1,25 +1,54 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title><?= TITRE ?></title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<meta name="Language" content="<?= LANG ?>"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<link href="<?= PATH_CSS ?>common.css" rel="stylesheet">
-		
-
-	</head> 
-	<body>
-		<!-- En-tête -->
-		<header class="" >
-			<section class="" >
-						<img src="<?= PATH_LOGO ?>" alt="<?= LOGO ?>" class="">
-						<h2><?= TITRE ?></h2>
-			</section>
-		</header>
-		<!-- Menu -->
-		<?php include(PATH_VIEWS.'menu.php'); ?>
-		<!-- Vue -->
-			<section class="">
-				
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?= TITLE ?></title>
+    <link href="./styles.css" rel="stylesheet" />
+  </head>
+  <body>
+    <nav class="navbar">
+      <button onclick="toggleMenu()" class="burger"></button>
+      <button class="button"><?= TITLE_HOME ?></button>
+      <div class="dropdowns">
+        <div class="dropdown">
+          <button class="button">
+            <?= TITLE_PURCHASE ?>
+            <img src="./assets/images/dropdown.svg" />
+          </button>
+          <div class="dropdown-menu">
+            <button>UX/UI Design</button>
+            <button>Web Applications</button>
+            <button>SEO Advice</button>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="button">
+            Products
+            <img src="chevron.svg" />
+          </button>
+          <div class="dropdown-menu">
+            <button>Learn CSS Ebook</button>
+            <button>Security Course</button>
+            <button>Masterclass Bundle</button>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="button">
+            Support
+            <img src="chevron.svg" />
+          </button>
+          <div class="dropdown-menu">
+            <button>Live Chat</button>
+            <button>Send Email</button>
+            <button>Request Help</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <script>
+      const toggleMenu = () => document.body.classList.toggle("open");
+    </script>
+  </body>
+</html>
