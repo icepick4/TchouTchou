2c665914db2f2031ed40352e9a562a5d055dd84d
icepick4 <remi.jara@laposte.net>
2022-11-14 10:30:59 +0100

    unlog -> logout + structure of single ticket

--- controllers/c_single_ticket.php
+++ controllers/c_single_ticket.php
@@ -2,8 +2,10 @@
 require_once(PATH_MODELS . 'user.php');
 
 if (!$_SESSION['logged']) {
-    $page = "login";
+    // $page = "login";
 } else {
     $user = new UserDAO(true);
     $ticket = $user->getSingleTicket($_SESSION['user_id'], $_GET['id']);
 }
+
+require_once(PATH_VIEWS . $page . '.php');
