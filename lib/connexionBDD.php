0e64b1432828bd071c3df302035998bf69582168
icepick4 <remi.jara@laposte.net>
2022-11-15 15:07:51 +0100

    cancel timeout on db

--- lib/connexionBDD.php
+++ lib/connexionBDD.php
@@ -2,7 +2,9 @@
 
 // Connects to the XE service (i.e. database) on the "localhost" machine
 $connect = '(DESCRIPTION=(ADDRESS= (PROTOCOL=TCP)(HOST=' . BD_HOST . ')(PORT=' . BD_PORT . ' ))(CONNECT_DATA = (SID =' . BD_SID . ')))';
-$conn = oci_connect(BD_USER, BD_PWD, $connect, 'AL32UTF8', OCI_SYSOPER);
+
+$conn = oci_connect(BD_USER, BD_PWD, $connect);
+
 
 if (!$conn) {
     $e = oci_error();
