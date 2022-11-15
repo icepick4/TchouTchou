5cf22e2bfb44e576cf53227c2c3771fdf5fa2cbe
Larcherbc <jules.rbc@gmail.com>
2022-11-15 09:44:19 +0100

    login avec nouvelle connexion bdd

--- controllers/c_login.php
+++ controllers/c_login.php
@@ -1,17 +1,26 @@
 <?php
-require_once(PATH_MODELS.'user.php');
+require_once(PATH_LIB.'connexionBDD.php');
 
 if (isset($_POST["mail"])&& isset($_POST["password"])) {
 
     $mail = $_POST["mail"];
 
-    $user = new UserDAO(true);
-    $result = $user->getUserMail($mail);
-    if (!empty($result)) {
-        if (password_verify($_POST["password"],$result['USER_PASSWORD'])) {
-            //$alert= choixAlert('connexion'); faire la gestion d'erreur
+    $request = 'SELECT * FROM USER_DATA where USER_MAIL = :mail';
+    $stid = oci_parse($conn, $request);
+
+    oci_bind_by_name($stid, ':mail', $mail);
+
+
+    oci_execute($stid);
+
+    $row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS);
+    print_r($row);
+
+    if (!empty($row)) {
+        if (password_verify($_POST["password"],$row['USER_PASSWORD'])) {
+            //$TODO : faire un notif quand connexion réussie
             $_SESSION ['logged'] = true;
-            $_SESSION['user_id'] = $result['USER_ID'];
+            $_SESSION['user_id'] = $row['USER_ID'];
         }else{
             $alert= choixAlert('unknown_password');
         }
