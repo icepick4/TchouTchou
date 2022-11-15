ca4625d25cd8e90528ee73c3d2ff32741fcc2366
Larcherbc <jules.rbc@gmail.com>
2022-11-15 14:50:42 +0100

    update connexionBDD.php with const

--- lib/connexionBDD.php
+++ lib/connexionBDD.php
@@ -1,9 +1,9 @@
 <?php
 
 // Connects to the XE service (i.e. database) on the "localhost" machine
-$connect='(DESCRIPTION=(ADDRESS= (PROTOCOL=TCP)(HOST=tchoutchou.ovh)(PORT=5521 ))(CONNECT_DATA = (SID =xe)))';
+$connect='(DESCRIPTION=(ADDRESS= (PROTOCOL=TCP)(HOST='.BD_HOST.')(PORT='.BD_PORT.' ))(CONNECT_DATA = (SID ='.BD_SID.')))';
 
-$conn = oci_connect('Tchou', 'Tchoutchou69', $connect);
+$conn = oci_connect(BD_USER, BD_PWD, $connect);
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
