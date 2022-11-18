5187cc73d212aba2c79e4b6e95a15cabf2bb921c
icepick4 <remi.jara@laposte.net>
2022-11-18 09:24:37 +0100

    adding spanish

---
+++ languages/es-ES.php
@@ -0,0 +1,53 @@
+<?php
+
+define('LOGO', 'Logo de la compañía'); // Mostrado si la imagen no se encuentra
+define('MENU_ACCUEIL', 'Inicio');
+define('TITRE_PAGE_ACCUEIL', 'Página de Inicio');
+
+//HEADER & FOOTER
+define('TITLE', 'Tchoutchou');
+define('TITLE_HOME', 'Inicio');
+define('TITLE_PURCHASE', 'Compras');
+define('TITLE_SEARCH', 'Buscar');
+define('TITLE_TICKET', 'Sus billetes');
+define('TITLE_LOGIN', 'Iniciar sesión');
+define('TITLE_INFORMATION', 'Información');
+define('TITLE_CONTACT', 'Contáctenos');
+define('TITLE_COMPANY', 'Nuestra Empresa');
+define('TITLE_INFORM_YOU', 'Informarle');
+define('TITLE_QUICK_ACCESS', 'Acceso rápido');
+define('STATION_INFORMATION', 'Estaciones & Plataformas');
+define('TERMS_OF_USE', 'Condiciones Generales');
+define('PRIVACY_POLICY', 'Política de Privacidad');
+
+// LOGIN & REGISTER
+define('LOGIN', 'Conexión');
+define('LOGOUT', 'Desconexión');
+define('REGISTER', 'Inscripción');
+define('TOREGISTER', "Inscribirse");
+define('INPUT_PASSWORD', 'Contraseña');
+define('INPUT_CONFIRMPASSWORD', 'Confirmar la contraseña');
+
+define('ERROR_NAME', 'El nombre no puede contener números');
+define('ERROR_FIRSTNAME', 'El apellido no puede contener números');
+define('ERROR_MAIL', 'Correo electrónico no válido');
+define('ERROR_PHONE', 'Número de teléfono no válido');
+define('ERROR_PASSWORD_TOO_WEAK', 'La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y debe tener al menos 8 caracteres');
+define('ERROR_PASSWORD_DIFFERENT', 'Las contraseñas son diferentes');
+define('ERROR_FORM', 'Corrija los errores antes de enviar sus datos');
+
+//contact
+define('INPUT_NAME', 'Nombre');
+define('INPUT_FIRST_NAME', 'Apellido');
+define('INPUT_PHONE', 'Teléfono');
+define('INPUT_EMAIL', 'Correo electrónico');
+define('INPUT_SUBJECT', 'Asunto');
+define('INPUT_DESC', 'Descripción');
+define('INPUT_SUBMIT', 'Enviar');
+
+// erreur
+define('TEXTE_PAGE_404', 'Oops, la página solicitada no existe!');
+define('MESSAGE_ERREUR', 'Se ha producido un error');
+define('UNKWOWN_MAIL', 'Correo electrónico desconocido');
+define('UNKNOWN_PASSWORD', 'Contraseña incorrecta');
+define('ERREUR_QUERY', 'Problema de acceso a la base de datos. Contacte al administrador');
--- languages/fr-FR.php
+++ languages/fr-FR.php
@@ -1,54 +1,53 @@
 <?php
 
 define('LOGO', 'Logo de la compagnie'); // Affiché si image non trouvée
-define('MENU_ACCUEIL','Accueil');
-define('TITRE_PAGE_ACCUEIL','Page D\'accueil');
+define('MENU_ACCUEIL', 'Accueil');
+define('TITRE_PAGE_ACCUEIL', 'Page D\'accueil');
 
 //HEADER & FOOTER
-define('TITLE','Tchoutchou');
-define('TITLE_HOME','Accueil');
-define('TITLE_PURCHASE','Achats');
-define('TITLE_SEARCH','Rechercher');
-define('TITLE_TICKET','Vos billets');
-define('TITLE_LOGIN','Se connecter');
-define('TITLE_INFORMATION','Informations');
-define('TITLE_CONTACT','Nous Contacter');
-define('TITLE_COMPANY','Notre Entreprise');
-define('TITLE_INFORM_YOU','Vous Informer');
-define('TITLE_QUICK_ACCESS','Accès Rapide');
-define('STATION_INFORMATION','Gares & Quais');
-define('TERMS_OF_USE','Condtions Générales');
-define('PRIVACY_POLICY','Politique de Confidentialité');
+define('TITLE', 'Tchoutchou');
+define('TITLE_HOME', 'Accueil');
+define('TITLE_PURCHASE', 'Achats');
+define('TITLE_SEARCH', 'Rechercher');
+define('TITLE_TICKET', 'Vos billets');
+define('TITLE_LOGIN', 'Se connecter');
+define('TITLE_INFORMATION', 'Informations');
+define('TITLE_CONTACT', 'Nous Contacter');
+define('TITLE_COMPANY', 'Notre Entreprise');
+define('TITLE_INFORM_YOU', 'Vous Informer');
+define('TITLE_QUICK_ACCESS', 'Accès Rapide');
+define('STATION_INFORMATION', 'Gares & Quais');
+define('TERMS_OF_USE', 'Condtions Générales');
+define('PRIVACY_POLICY', 'Politique de Confidentialité');
 
 // LOGIN & REGISTER
-define('LOGIN','Connexion');
-define('LOGOUT','Déconnexion');
-define('REGISTER','Inscription');
-define('TOREGISTER',"S'inscrire");
-define('INPUT_PASSWORD','Mot de passe');
-define('INPUT_CONFIRMPASSWORD','Confirmer le mot de passe');
+define('LOGIN', 'Connexion');
+define('LOGOUT', 'Déconnexion');
+define('REGISTER', 'Inscription');
+define('TOREGISTER', "S'inscrire");
+define('INPUT_PASSWORD', 'Mot de passe');
+define('INPUT_CONFIRMPASSWORD', 'Confirmer le mot de passe');
 
-define('ERROR_NAME','Le nom ne peut contenir de nombre');
-define('ERROR_FIRSTNAME','Le prénom ne peut contenir de nombre');
-define('ERROR_MAIL','Mail invalide');
-define('ERROR_PHONE','Numéro de téléphone invalide');
-define('ERROR_PASSWORD_TOO_WEAK','Le mot de passe doit contenir au moins une lettre majuscule, une minuscule, un chiffre et être composé d\'au moins 8 caractères');
-define('ERROR_PASSWORD_DIFFERENT','Les mots de passe sont différents');
-define('ERROR_FORM','Veuillez corriger les erreurs avant d\'envoyer vos informations');
+define('ERROR_NAME', 'Le nom ne peut contenir de nombre');
+define('ERROR_FIRSTNAME', 'Le prénom ne peut contenir de nombre');
+define('ERROR_MAIL', 'Mail invalide');
+define('ERROR_PHONE', 'Numéro de téléphone invalide');
+define('ERROR_PASSWORD_TOO_WEAK', 'Le mot de passe doit contenir au moins une lettre majuscule, une minuscule, un chiffre et être composé d\'au moins 8 caractères');
+define('ERROR_PASSWORD_DIFFERENT', 'Les mots de passe sont différents');
+define('ERROR_FORM', 'Veuillez corriger les erreurs avant d\'envoyer vos informations');
 
 //contact
 define('INPUT_NAME', 'Nom');
-define('INPUT_FIRST_NAME','Prénom');
-define('INPUT_PHONE','Numéro de téléphone');
-define('INPUT_EMAIL','Email');
-define('INPUT_SUBJECT','Sujet');
-define('INPUT_DESC','Description');
-define('INPUT_SUBMIT','Envoyer');
+define('INPUT_FIRST_NAME', 'Prénom');
+define('INPUT_PHONE', 'Numéro de téléphone');
+define('INPUT_EMAIL', 'Email');
+define('INPUT_SUBJECT', 'Sujet');
+define('INPUT_DESC', 'Description');
+define('INPUT_SUBMIT', 'Envoyer');
 
 // erreur
-define('TEXTE_PAGE_404','Oops, la page demandée n\'existe pas !');
-define('MESSAGE_ERREUR',"Une erreur s'est produite");
-define('UNKWOWN_MAIL','Mail inconnu');
-define('UNKNOWN_PASSWORD','Mot de passe incorrect');
+define('TEXTE_PAGE_404', 'Oops, la page demandée n\'existe pas !');
+define('MESSAGE_ERREUR', "Une erreur s'est produite");
+define('UNKWOWN_MAIL', 'Mail inconnu');
+define('UNKNOWN_PASSWORD', 'Mot de passe incorrect');
 define('ERREUR_QUERY', 'Problème d\'accès à la base de données. Contactez l\'administrateur');
-?>
