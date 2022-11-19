0791ac78a5e08e7afc72c4cedc1cbdb6bddceb3c
Qypol342 <Qypol342@gmail.com>
2022-11-19 18:12:51 +0100

    [FIX] Finaly git auto pull good to go

--- pull.php
+++
@@ -1,6 +0,0 @@
-<?php echo shell_exec( 'cd /home/vo269ma/sae-sncf; git reset --hard origin/prod; git clean -d -f; git pull https://gitlab-ci-token:glpat-wFygfmCs9RddfneY2Zvo@forge.univ-lyon1.fr/p2103642/sae-sncf.git main');
-
-echo "good?";
-
-echo shell_exec( 'ls');
-shell_exec( 'touch working') ?>
