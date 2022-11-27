af0eb5ce26aa538e3976787701ed42c5582a8116
Larcherbc <jules.rbc@gmail.com>
2022-11-27 09:26:57 +0100

    add password show function

--- views/v_login.php
+++ views/v_login.php
@@ -4,6 +4,7 @@
 <?php require_once(PATH_VIEWS . 'header.php'); ?>
 
 <script src=<?= PATH_JS . 'login.js' ?> type="module" defer></script>
+<script src=<?= PATH_JS . 'passwordShow.js' ?> type="module" defer></script>
 <!--  Zone message d'alerte -->
 <?php require_once(PATH_VIEWS . 'alert.php'); ?>
 
@@ -15,7 +16,7 @@
     <form method="post" action="index.php?page=login">
         <label id="info"><?= ERROR_MAIL ?></label>
         <input type="mail" name="mail" id="email" placeholder="<?= EMAIL ?>"></input>
-        <input type="password" name="password" id="password" placeholder="<?= PASSWORD ?>"></input>
+        <div><input type="password" name="password" id="password" placeholder="<?= PASSWORD ?>"></input><i class="show-password"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15 12c0 1.654-1.346 3-3 3s-3-1.346-3-3 1.346-3 3-3 3 1.346 3 3zm9-.449s-4.252 8.449-11.985 8.449c-7.18 0-12.015-8.449-12.015-8.449s4.446-7.551 12.015-7.551c7.694 0 11.985 7.551 11.985 7.551zm-7 .449c0-2.757-2.243-5-5-5s-5 2.243-5 5 2.243 5 5 5 5-2.243 5-5z"/></svg></i></div>
         <input type="submit" value="Login"></input>
     </form>
     <div class="links">
