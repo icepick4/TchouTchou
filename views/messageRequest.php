67de95269937ad3c7d9117f4a9b979c1df0b2224
Larcherbc <jules.rbc@gmail.com>
2022-11-28 10:02:01 +0100

    small fix alert and try to use global config for ajax

--- views/messageRequest.php
+++ views/messageRequest.php
@@ -6,6 +6,13 @@
 
 <?php
 
+//pour utiliser le fichier de config de base
+// $skipSession = true;
+// require_once('../config/configuration.php');
+// require_once('.'.PATH_MODELS . 'UserDAO.php');
+
+
+
 $id = intval($_GET['number']);
 
 $request = 'SELECT * FROM DISCUSSION_SUPPORT WHERE DISCUSSION_ID=:id ORDER BY DISCUSSION_ID DESC';
@@ -24,6 +31,7 @@ $connect = '(DESCRIPTION=(ADDRESS= (PROTOCOL=TCP)(HOST=tchoutchou.ovh)(PORT=5521
         oci_execute($stid);
         $i = 0;
         $result = array();
+
         while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
             $result[$i] = $row;
             $i++;
