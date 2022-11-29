9e0c79160ec69a89bdfcb529f55cf1806350944d
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2022-11-29 09:15:16 +0100

    Doc of UserDAO

--- models/UserDAO.php
+++ models/UserDAO.php
@@ -4,18 +4,38 @@ require_once(PATH_MODELS . 'DAO.php');
 
 class UserDAO extends DAO
 {
+    /**
+     * Function to get the user's data by mail
+     * @param string $mail The user's mail
+     * @return array The user's data
+     *
+     */
     public function getUserByMail($mail)
     {
         $sql = 'SELECT * FROM USER_DATA where USER_MAIL = :mail';
         $args = array(':mail' => $mail,);
         return $this->queryRow($sql, $args);
     }
+
+    /**
+     * Function to get the user data by the user id
+     * @param number $id The user's id
+     * @return array The user's data
+     *
+     */
     public function getUserById($id)
     {
         $sql = 'SELECT * FROM USER_DATA where USER_ID = :id';
         $args = array(':id' => $id,);
         return $this->queryRow($sql, $args);
     }
+
+    /**
+     * Function to get all user's tickets
+     * @param number $id The user's id
+     * @return array The user's tickets
+     * 
+     */
     public function getTicketsById($id)
     {
         $sql = 'SELECT (SELECT STATION_NAME FROM STATION S1 WHERE S1.STATION_ID = T.START_STATION_ID) START_STATION_NAME, 
@@ -26,6 +46,14 @@ class UserDAO extends DAO
         $args = array(':id' => $id,);
         return $this->queryAll($sql, $args);
     }
+
+    /**
+     * Function to get a ticket by id and user id
+     * @param number $id The ticket's id
+     * @param number $userId The user's id
+     * @return array The ticket's data
+     *
+     */
     public function getTicketById($id, $user_id)
     {
         $sql = 'SELECT (SELECT STATION_NAME FROM STATION S1 WHERE S1.STATION_ID = T.START_STATION_ID) START_STATION_NAME,
@@ -39,23 +67,52 @@ class UserDAO extends DAO
         $args = array(':id' => $id, ':user_id' => $user_id);
         return $this->queryRow($sql, $args);
     }
+
+    /**
+     * Function to get all the users
+     * @return array The users's data
+     *
+     */
     public function getAllUser()
     {
         $sql = 'SELECT * FROM USER_DATA';
         return $this->queryAll($sql);
     }
+
+    /**
+     * Function to update the user's first name
+     * @param number $id The user's id
+     * @param string $firstName
+     * @return array 
+     * 
+     */
     public function updateFirstName($id, $first_name)
     {
         $sql = 'UPDATE USER_DATA SET USER_FIRSTNAME = :first_name WHERE USER_ID = :id';
         $args = array(':id' => $id, ':first_name' => $first_name);
-        return $this->queryInsert($sql, $args);
+        $this->queryInsert($sql, $args);
     }
+
+    /**
+     * Function to update the user's last name
+     * @param number $id
+     * @param string $last_name
+     * @return array
+     *
+     */
     public function updateLastName($id, $last_name)
     {
         $sql = 'UPDATE USER_DATA SET USER_LASTNAME = :last_name WHERE USER_ID = :id';
         $args = array(':id' => $id, ':last_name' => $last_name);
         return $this->queryInsert($sql, $args);
     }
+
+    /**
+     * Function to update the user's phone number
+     * @param number $id
+     * @param string $phone_number
+     * @return array
+     */
     public function updatePhone($id, $phone)
     {
         $sql = 'UPDATE USER_DATA SET USER_PHONE = :phone WHERE USER_ID = :id';
@@ -63,7 +120,7 @@ class UserDAO extends DAO
         return $this->queryInsert($sql, $args);
     }    
     /**
-     * Function to update the mail address of a user
+     * Function to update the user's mail address
      *
      * @param  number $id
      * @param  string $mail
@@ -107,11 +164,26 @@ class UserDAO extends DAO
         $this->queryInsert($sql, $args);
     }
 
+    /**
+     * Function to hash the password of a user
+     *
+     * @param  number $id
+     * @param  string $password
+     * @return string
+     *
+     */
     public function hashPassword($password)
     {
         return password_hash($password, PASSWORD_DEFAULT);
     }
 
+    /**
+     * Function to get the category of a user
+     *
+     * @param  number $id
+     * @return array
+     *
+     */
     public function getUserType($id)
     {
         $sql = 'SELECT USER_CATEGORIE_ID FROM USER_DATA WHERE USER_ID = :id';
@@ -119,16 +191,37 @@ class UserDAO extends DAO
         return $this->queryRow($sql, $args)['USER_CATEGORIE_ID'];
     }
 
+    /**
+     * Function to check if the user is an employee
+     *
+     * @param  number $id
+     * @return boolean
+     *
+     */
     public function isEmployee($id)
     {
         return $this->getUserType($id) == 1;
     }
 
+    /**
+     * Function to check if the user is a customer
+     *
+     * @param  number $id
+     * @return boolean
+     *
+     */
     public function isCustomer($id)
     {
         return $this->getUserType($id) == 0;
     }
 
+    /**
+     * Function to get the employee type of an employee
+     *
+     * @param  number $id
+     * @return array
+     *
+     */
     public function getEmployeeType($id)
     {
         $sql = 'SELECT EMPLOYEE_ACCESS FROM EMPLOYEES_DATA WHERE USER_ID = :id';
@@ -136,26 +229,51 @@ class UserDAO extends DAO
         return $this->queryRow($sql, $args)['EMPLOYEE_ACCESS'];
     }
 
+    /**
+     * Function to check if the employee is a manager
+     *
+     * @param  number $id
+     * @return boolean
+     *
+     */
     public function isAdministrator($id)
     {
         return $this->getEmployeeType($id) == 1;
     }
 
+    /**
+     * Function to check if the employee is a station
+     *
+     * @param  number $id
+     * @return boolean
+     *
+     */
     public function isStation($id)
     {
         return $this->getEmployeeType($id) == 2;
     }
 
+    /**
+     * Function to check if the employee is a driver
+     *
+     * @param  number $id
+     * @return boolean
+     *
+     */
     public function isDriver($id)
     {
         return $this->getEmployeeType($id) == 3;
     }
 
+    /**
+     * Function to check if the employee is a service
+     *
+     * @param  number $id
+     * @return boolean
+     *
+     */
     public function isService($id)
     {
         return $this->getEmployeeType($id) == 4;
     }
-
-
-    // 0
 }
