4a6491f51967d40b270daba7ece8161258180e9c
Larcherbc <jules.rbc@gmail.com>
2022-12-13 09:00:29 +0100

    add train page

---
+++ assets/js/train.js
@@ -0,0 +1,5 @@
+let svg = document.querySelector("#train");
+svg.addEventListener("click", function (e) {
+  console.log(e.target);
+});
+console.log(svg);
---
+++ controllers/c_train.php
@@ -0,0 +1,4 @@
+<?php
+
+
+require_once(PATH_VIEWS . $page . '.php');
