d1e7f137162f223038d5ac9ab89b2797e0ba6ee0
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2022-12-17 22:57:16 +0100

    Adding home page when disconnect

--- views/v_home.php
+++ views/v_home.php
@@ -7,14 +7,41 @@
 <?php
 if (!$_SESSION['logged']) {
 ?>
-    <div class="content">
-        <div class="links">
-            <a href="index.php?page=login"><?= LOGIN ?></a>
-            <a href="index.php?page=register"><?= REGISTER ?></a>
-        </div>
-    </div>
+	<article>
+		<img src="assets/images/travel.svg">
+		<div class="arti">
+			<h2><?=WELCOME?></h2>
+			<p><?=WELCOME_TEXT?></p>
+		</div>
+	</article>
+	<article>
+		<img src="assets/images/destination.svg">
+		<div class="arti">
+			<h2><?=TITLE_SEARCH?></h2>
+			<p><?=PLATFORM_MANAGER_TEXT?></p>
+			<a href="index.php?page=purchase">
+				<div class="btn">
+					<p><?=WELCOME_GO?></p>
+					<img src="assets/images/arrow.svg">
+				</div>
+			</a>
+		</div>
+	</article>
+	<article>
+		<img src="assets/images/search.svg">
+		<div class="arti">
+			<h2><?= STATION_LIST ?></h2>
+			<p><?= MAINTENANCE_TEXT ?></p>
+            <a href="index.php?page=station_list">
+			<div class="btn">
+				<p><?= WELCOME_GO ?></p>
+				<img src="assets/images/arrow.svg">
+			</div></a>
+		</div>
+	</article>
+
 <?php
-} else { ?>
+    } else { ?>
     <div class="content">
         <h1 id="title"><?= WELCOME ?><?php echo $_SESSION['first_name'] ?> !</h1>
         <div class="links">
