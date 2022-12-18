f1000506caa39fd4028cd8ec2c7eb7814556e010
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2022-12-19 00:36:53 +0100

    Fix home page

--- views/v_home.php
+++ views/v_home.php
@@ -10,43 +10,7 @@
 
 <!-- Début de la page -->
 
-<?php if ($user->isEmployee($_SESSION['user_id'])) {?>
-	<article>
-		<img src="assets/images/work_il.svg">
-		<div class="arti">
-			<h2><?=WELCOME?></h2>
-			<p><?=WELCOME_TEXT?></p>
-		</div>
-	</article>
-	<?php if($user->isStation($_SESSION['user_id'])) { ?>
-		<article>
-			<img src="assets/images/station_il.svg">
-			<div class="arti">
-				<h2><?=PLATFORM_MANAGER?></h2>
-				<p><?=PLATFORM_MANAGER_TEXT?></p>
-				<a href="index.php?page=platform_manager">
-					<div class="btn">
-						<p><?=WELCOME_GO?></p>
-						<img src="assets/images/arrow.svg">
-					</div>
-				</a>
-			</div>
-		</article>
-	<?php } if($user->isService($_SESSION['user_id'])) { ?>
-		<article>
-			<img src="assets/images/engineer_il.svg">
-			<div class="arti">
-				<h2><?=MAINTENANCE?></h2>
-				<p><?=MAINTENANCE_TEXT?></p>
-				<a href="index.php?page=maintenance">
-				<div class="btn">
-					<p><?=WELCOME_GO?></p>
-					<img src="assets/images/arrow.svg">
-				</div></a>
-			</div>
-		</article>
-<?php }
-} else { ?>
+<?php if (!isset($_SESSION['user_id']) || !$user->isEmployee($_SESSION['user_id'])) { ?>
 	<article>
 		<img src="assets/images/travel.svg">
 		<div class="arti">
@@ -79,8 +43,45 @@
 			</div></a>
 		</div>
 	</article>
-
-<?php } ?>
+<?php } else { ?>
+	<article>
+		<img src="assets/images/work_il.svg">
+		<div class="arti">
+			<h2><?= WELCOME ?></h2>
+			<p><?= WELCOME_TEXT ?></p>
+		</div>
+	</article>
+	<?php if ($user->isStation($_SESSION['user_id'])) { ?>
+		<article>
+			<img src="assets/images/station_il.svg">
+			<div class="arti">
+				<h2><?= PLATFORM_MANAGER ?></h2>
+				<p><?= PLATFORM_MANAGER_TEXT ?></p>
+				<a href="index.php?page=platform_manager">
+					<div class="btn">
+						<p><?= WELCOME_GO ?></p>
+						<img src="assets/images/arrow.svg">
+					</div>
+				</a>
+			</div>
+		</article>
+	<?php }
+	if ($user->isService($_SESSION['user_id'])) { ?>
+		<article>
+			<img src="assets/images/engineer_il.svg">
+			<div class="arti">
+				<h2><?= MAINTENANCE ?></h2>
+				<p><?= MAINTENANCE_TEXT ?></p>
+				<a href="index.php?page=maintenance">
+				<div class="btn">
+					<p><?= WELCOME_GO ?></p>
+					<img src="assets/images/arrow.svg">
+				</div></a>
+			</div>
+		</article>
+<?php }
+}
+	?>
 <!--  Fin de la page -->
 
 <!--  Pied de page -->
