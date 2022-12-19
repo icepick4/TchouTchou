ef27355f3ab350d6b3bf2557b0e830c825614921
Larcherbc <jules.rbc@gmail.com>
2022-12-19 12:14:48 +0100

    split js function in two parts

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -7,15 +7,19 @@ let flag = script.outerHTML;
 
 //event listener for each discussion_resume
 for (let i = 0; i < message.length; i++) {
-  message[i].addEventListener("click", loadMessage);
+  message[i].addEventListener("click", getMessageIdOnclick);
 }
 
-function loadMessage(e) {
+function getMessageIdOnclick(e) {
   if (e.target.nodeName == "H3" || e.target.nodeName == "P") {
     var number = e.target.parentNode.children[0].innerText;
   } else {
     var number = e.target.children[0].innerText;
   }
+  loadMessage(number);
+}
+
+function loadMessage(id) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -24,7 +28,7 @@ function loadMessage(e) {
   };
   xmlhttp.open(
     "GET",
-    "views/messageRequest.php?number=" + number + "&id=" + getFlags(flag)[0],
+    "views/messageRequest.php?number=" + id + "&id=" + getFlags(flag)[0],
     true
   );
   xmlhttp.send();
