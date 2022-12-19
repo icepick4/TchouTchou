2ae6db35b8417c9872f8f00e05df213944bf1aa3
Larcherbc <jules.rbc@gmail.com>
2022-12-19 13:39:56 +0100

    add verification to access maintenance page

--- controllers/c_maintenance.php
+++ controllers/c_maintenance.php
@@ -1,16 +1,20 @@
 <?php
 require_once(PATH_MODELS . 'TrainDAO.php');
-
+require_once(PATH_MODELS . 'UserDAO.php');
 $train = new TrainDAO();
+$user = new UserDAO();
 
 $trains = $train->get_trains();
-
-if(isset($_POST['status'])){
+if ($_SESSION['logged'] && $user->isService($_SESSION['user_id'])) {
+    if(isset($_POST['status'])){
     
-    $status = $_POST['status'];
-    $train_id = $_POST['train_id'];
-    $train->update_train_status($status, $train_id);
-    header('Location: index.php?page=maintenance');
+        $status = $_POST['status'];
+        $train_id = $_POST['train_id'];
+        $train->update_train_status($status, $train_id);
+    }
+} else {
+    header("Location: index.php?page=home");
+    die();
 }
 
 require_once(PATH_VIEWS . $page . '.php');
