466ef3503e6b322e6bfee8f9a1a1f300d034d2e8
Larcherbc <jules.rbc@gmail.com>
2022-12-27 14:03:04 +0100

    fix hour

--- controllers/c_traffic_alert.php
+++ controllers/c_traffic_alert.php
@@ -13,11 +13,11 @@ if(isset($_POST['alertType']) && isset($_POST['message']) && isset($_POST['infop
     $alerts = new AlertDAO();
 
     $driver = $user->getDriverID($_SESSION['user_id']);
-    $travel = $planning->getDriverTravelForTheDayByHour($driver, 22);
+    $travel = $planning->getDriverTravelForTheDayByHour($driver, $date('H'));
 
     $alerts= $alerts->createAlert($travel[0]['TRAVEL_ID'], $_POST['alertType'], $_POST['message'], $_POST['infoposition']);
     $alert = choixAlert('alert_created');
-    
+
 }
 
 require_once(PATH_VIEWS.$page.'.php');
