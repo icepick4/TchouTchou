8f71fab6599cbae5c9d4d5f2b9d36ac3a655876b
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2022-12-30 22:25:48 +0100

    Update for alert process

--- assets/js/alert_single.js
+++ assets/js/alert_single.js
@@ -1,7 +1,13 @@
 let buttonToDesactive = document.getElementById("finishButton");
-
+let url = window.location.href;
 buttonToDesactive.addEventListener("click", function () {
   var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "views/endAlert.php", true);
+  xhttp.open(
+    "GET",
+    "views/endAlert.php?id=" + url.slice(url.indexOf("=") + 1),
+    true
+  );
   xhttp.send();
+  xhttp.onload = function () {};
+  xhttp.onerror = function () {};
 });
