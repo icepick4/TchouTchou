62562b1d4a8ab6a23e9dfc81fbed5d1c9b7ab5b4
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-01-02 09:51:11 +0100

    Multi updates in payment

--- views/v_buy_payment.php
+++ views/v_buy_payment.php
@@ -1,7 +1,6 @@
 <?php require_once(PATH_VIEWS . 'header.php'); ?>
-<script src="https://www.paypal.com/sdk/js?client-id=test&currency=USD"></script>
-<!-- Set up a container element for the button -->
-<div id="paypal-button-container"></div>
+<script src="https://www.paypal.com/sdk/js?client-id=test&currency=EUR"></script>
+
 <script>
   paypal.Buttons({
     // Sets up the transaction when a payment button is clicked
@@ -29,4 +28,59 @@
     }
   }).render('#paypal-button-container');
 </script>
+<div id="travel-payment">
+  <div id="travel-details">
+    <h1>Détails de votre voyage à <span><?= $station_name ?></h1>
+    <div id="travel_details">
+        <p><?php 
+            if (date('l',strtotime($ticket[0]['DEPARTURE_DATE'])) == "Monday") {
+                echo MONDAY;
+            } else if (date('l',strtotime($ticket[0]['DEPARTURE_DATE'])) == "Tuesday") {
+                echo TUESDAY;
+            } else if (date('l',strtotime($ticket[0]['DEPARTURE_DATE'])) == "Wednesday") {
+                echo WEDNESDAY;
+            } else if (date('l',strtotime($ticket[0]['DEPARTURE_DATE'])) == "Thursday") {
+                echo THURSDAY;
+            } else if (date('l',strtotime($ticket[0]['DEPARTURE_DATE'])) == "Friday") {
+                echo FRIDAY;
+            } else if (date('l',strtotime($ticket[0]['DEPARTURE_DATE'])) == "Saturday") {
+                echo SATURDAY;
+            } else {
+                echo SUNDAY;
+            }
+            if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "January") {
+                echo JANUARY;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "February") {
+                echo FEBRUARY;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "March") {
+                echo MARCH;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "April") {
+                echo APRIL;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "May") {
+                echo MAY;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "June") {
+                echo JUNE;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "July") {
+                echo JULY;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "August") {
+                echo AUGUST;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "September") {
+                echo SEPTEMBER;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "October") {
+                echo OCTOBER;
+            } else if (date('F',strtotime($ticket[0]['DEPARTURE_DATE'])) == "November") {
+                echo NOVEMBER;
+            } else {
+                echo DECEMBER;
+            }
+     ?></p>
+        <p><span class="colored"><?php echo $ticket[0]['DEPARTURE_TIME']?></span> ● <?= $from?></p>
+        <p>Voyage n°<span class="colored"><?php echo $ticket[0]['TRAVEL_ID']?></span></p>
+        <p><span class="colored"><?php echo $ticket[0]['END_TIME']?></span> ● <?= $to?></p>
+    </div>
+  </div>
+  <div id="paypal-button-container">
+    <p><?= $_POST['price'] ?> €</p>
+  </div>
+</div>
 <?php require_once(PATH_VIEWS . 'footer.php');
