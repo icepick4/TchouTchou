7ae76225b17f01636c55ee05fb7099a4c61b43b6
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-01-05 19:16:58 +0100

    Allow administraor's access to alert

--- controllers/c_alert_single.php
+++ controllers/c_alert_single.php
@@ -2,7 +2,7 @@
 require_once(PATH_MODELS.'AlertDAO.php');
 require_once(PATH_MODELS.'StaffDAO.php');
 $staff = new StaffDAO();
-if ($_SESSION['logged'] && $staff->isService($_SESSION['user_id']) || $staff->isStation($_SESSION['user_id'])) {
+if ($_SESSION['logged'] && $staff->isService($_SESSION['user_id']) || $staff->isStation($_SESSION['user_id']) || $staff->isAdministrator($_SESSION['user_id'])) {
     
 
     $alerts = new AlertDAO();
