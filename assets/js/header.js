35cdaccbc9c8d1bfa0a1baae775fe255bce167f9
Icepick <remi.jara@laposte.net>
2023-01-07 15:25:30 +0100

    phone repsonsive design on account

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,13 +4,13 @@ arrow.addEventListener('click', function () {
     let ul = document.querySelector('header nav ul');
     let nav = document.querySelector('header nav:first-child');
     rotateArrow();
-    if (ul.style.display === 'block') {
+    if (ul.style.display === 'flex') {
         ul.style.display = 'none';
         nav.style.flexDirection = 'row';
         return;
     }
     nav.style.flexDirection = 'column';
-    ul.style.display = 'block';
+    ul.style.display = 'flex';
 });
 
 function rotateArrow() {
--- views/v_buy_payment.php
+++ views/v_buy_payment.php
@@ -22,37 +22,39 @@
         var xhttp = new XMLHttpRequest();
         xhttp.open(
           "GET",
-          link ,
+          link,
           true
         );
         xhttp.send();
-        xhttp.onload = function () {
-        if (this.status == 200) {
-          window.location.href = '?page=ticket_list';
-        }
+        xhttp.onload = function() {
+          if (this.status == 200) {
+            window.location.href = '?page=ticket_list';
+          }
         };
         // When ready to go live, remove the alert and show a success message within this page. For example:
         // const element = document.getElementById('paypal-button-container');
         // element.innerHTML = '<h3>Thank you for your payment!</h3>';
         // Or go to another URL:  actions.redirect('thank_you.html');
 
-        
+
       });
     }
   }).render('#paypal-button-container');
 </script>
+<script src=<?= PATH_JS . 'buy_payment.js' ?> type="module" defer></script>
+
 <div id="travel-payment">
   <div id="travel-details">
     <h1>Détails de votre voyage vers <span><?= $to_station_name ?></h1>
     <div id="travel_details">
-        <p><?php 
-            echo getDay($from_station_date_departure);
-            echo ' '.date('d',strtotime($from_station_date_departure)).' ';
-        echo getMonth($from_station_date_departure);
-     ?></p>
-        <p><span class="colored"><?= $from_station_time_departure ?></span> ● <?= $from_station_name?></p>
-        <p>Voyage n°<span class="colored"><?= $_POST['travel']?></span></p>
-        <p><span class="colored"><?= $to_station_time_arrival ?></span> ● <?= $to_station_name?></p>
+      <p><?php
+          echo getDay($from_station_date_departure);
+          echo ' ' . date('d', strtotime($from_station_date_departure)) . ' ';
+          echo getMonth($from_station_date_departure);
+          ?></p>
+      <p><span class="colored"><?= $from_station_time_departure ?></span> ● <?= $from_station_name ?></p>
+      <p>Voyage n°<span class="colored"><?= $_POST['travel'] ?></span></p>
+      <p><span class="colored"><?= $to_station_time_arrival ?></span> ● <?= $to_station_name ?></p>
     </div>
   </div>
   <div id="paypal-button-container">
