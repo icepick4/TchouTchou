688e64d82d7cd7d3414db391ad4941d837bb93ce
Icepick <remi.jara@laposte.net>
2023-01-07 16:26:14 +0100

    responsive on aller_list/maintenance/staff(not finished) + fix style on home/buy_payment

--- views/v_staff.php
+++ views/v_staff.php
@@ -8,7 +8,7 @@
 <?php
 if ($staff_list != null) {
 ?>
-    
+
     <div class="container">
         <input type="text" id="search" placeholder="<?= SEARCH ?>">
 
@@ -41,49 +41,50 @@ if ($staff_list != null) {
                 <td COLSPAN=2>
                     <form method="post" action="index.php?page=staff">
                         <select name="staff_type_update" id="staff_type_update" class="status">
+                            <option></option>
                             <option value="1" <?php if ($staff['EMPLOYEE_CATEGORIE_ID'] == 1) echo 'selected' ?>><?= ADMINISTRATOR ?></option>
                             <option value="2" <?php if ($staff['EMPLOYEE_CATEGORIE_ID'] == 2) echo 'selected' ?>><?= STATION ?></option>
                             <option value="3" <?php if ($staff['EMPLOYEE_CATEGORIE_ID'] == 3) echo 'selected' ?>><?= DRIVER ?></option>
                             <option value="4" <?php if ($staff['EMPLOYEE_CATEGORIE_ID'] == 4) echo 'selected' ?>><?= SERVICE ?></option>
                             <option value="55" <?php if ($staff['EMPLOYEE_CATEGORIE_ID'] == 5) echo 'selected' ?>><?= SUPPORT ?></option>
                         </select>
-                        <input type="text" id="user_identity_update" name="user_identity_update" value="<?= $staff['USER_ID'] ?> " style="display:none;" ></input>
+                        <input type="text" id="user_identity_update" name="user_identity_update" value="<?= $staff['USER_ID'] ?> " style="display:none;"></input>
                         <input class='links' type="submit" value=<?= VALIDATE ?>></input>
                     </form>
                 </td>
                 <td>
-                    <button value=<?= $staff['USER_ID']?> class="fired-button">✖</button>
+                    <button value=<?= $staff['USER_ID'] ?> class="fired-button">✖</button>
                 </td>
             </tr>
         <?php
         }
         ?>
     </table>
-    
-<?php
-}?>
-<form method="post" action="index.php?page=staff">
-<fieldset>
-    <legend><?= ADD_STAFF?></legend>
-    <div>
-        <label for="user_identity"><?= IDENTITY ?></label>
-        <select name="user_identity_create" id="user_identity_create" class="user_identity">
-        <?php foreach ($user_list_not_employees as $user) { ?>
-                                <option value=<?=$user['USER_ID']?>><?=  $user['USER_FIRSTNAME'].' '.$user['USER_LASTNAME'] ?></option>
-                            <?php } ?>
-        </select>
-    </div>
-    <div>
-    <label for="staff_type"><?= TYPE ?></label>
-        <select name="staff_type_create" id="staff_type_create" class="staff_type">
-        <?php foreach ($staff_type as $staff_type) { ?>
-                                <option value=<?=$staff_type['EMPLOYEE_CATEGORIE_ID']?>><?=  $staff_type['EMPLOYEE_CATEGORIE_LABEL'] ?></option>
-                            <?php } ?>
-        </select>
-    </div>
-    <input class='links' type="submit" value=<?= BTN_ADD ?>></input>
 
-    </form>
+<?php
+} ?>
+<form method="post" action="index.php?page=staff">
+    <fieldset>
+        <legend><?= ADD_STAFF ?></legend>
+        <div>
+            <label for="user_identity"><?= IDENTITY ?></label>
+            <select name="user_identity_create" id="user_identity_create" class="user_identity">
+                <?php foreach ($user_list_not_employees as $user) { ?>
+                    <option value=<?= $user['USER_ID'] ?>><?= $user['USER_FIRSTNAME'] . ' ' . $user['USER_LASTNAME'] ?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <div>
+            <label for="staff_type"><?= TYPE ?></label>
+            <select name="staff_type_create" id="staff_type_create" class="staff_type">
+                <?php foreach ($staff_type as $staff_type) { ?>
+                    <option value=<?= $staff_type['EMPLOYEE_CATEGORIE_ID'] ?>><?= $staff_type['EMPLOYEE_CATEGORIE_LABEL'] ?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <input class='links' type="submit" value=<?= BTN_ADD ?>></input>
+
+</form>
 
 
 
