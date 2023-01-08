02392a6bb295404453da0051c95651127d40d662
Larcherbc <jules.rbc@gmail.com>
2023-01-08 14:56:36 +0100

    add sytle for modal in planning_driver

--- assets/js/planning_driver.js
+++ assets/js/planning_driver.js
@@ -1,18 +1,34 @@
 document.querySelectorAll(".travel").forEach((travel) => {
   travel.addEventListener("click", (e) => {
-    console.log(e.target);
     let id = e.target.getAttribute("value");
     openModal(id);
   });
 });
 
+let modal = document.getElementById("infoModal");
+let overlay = document.getElementById("overlay");
+
 function openModal(id) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("modal").innerHTML = this.responseText;
+      modal.children[1].innerHTML = this.responseText;
+      modal.classList.add("active");
+      overlay.classList.add("active");
     }
   };
   xmlhttp.open("GET", "ajax/planning_info.php?id=" + id, true);
   xmlhttp.send();
+}
+
+document.querySelectorAll(".close").forEach((close) => {
+  close.addEventListener("click", (e) => {
+    closeModal();
+  });
+});
+
+function closeModal() {
+  modal.classList.remove("active");
+
+  overlay.classList.remove("active");
 }
