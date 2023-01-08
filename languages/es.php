a9a51a0fd17997617bf1b20f2c84c860a983ce75
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-01-08 20:47:28 +0100

    Reactualise spanish file

--- languages/es.php
+++ languages/es.php
@@ -1,53 +1,270 @@
 <?php
 
-define('LOGO', 'Logo de la compañía'); // Mostrado si la imagen no se encuentra
-define('MENU_ACCUEIL', 'Inicio');
-define('TITRE_PAGE_ACCUEIL', 'Página de Inicio');
-
-//HEADER & FOOTER
+// TITLE
 define('TITLE', 'Tchoutchou');
 define('TITLE_HOME', 'Inicio');
 define('TITLE_PURCHASE', 'Compras');
 define('TITLE_SEARCH', 'Buscar');
-define('TITLE_TICKET', 'Sus billetes');
-define('TITLE_LOGIN', 'Iniciar sesión');
 define('TITLE_INFORMATION', 'Información');
-define('TITLE_CONTACT', 'Contáctenos');
+define('TITLE_CONTACT', 'Contáctanos');
 define('TITLE_COMPANY', 'Nuestra Empresa');
-define('TITLE_INFORM_YOU', 'Informarle');
-define('TITLE_QUICK_ACCESS', 'Acceso rápido');
-define('STATION_INFORMATION', 'Estaciones & Plataformas');
-define('TERMS_OF_USE', 'Condiciones Generales');
-define('PRIVACY_POLICY', 'Política de Privacidad');
+define('TITLE_INFORM_YOU', 'Informarte');
+define('TITLE_QUICK_ACCESS', 'Acceso Rápido');
+define('TITLE_PLATFORM_MANAGER', 'Gestión de Andenes');
+define('TITLE_TERMS_OF_USE', 'Términos y Condiciones');
+define('TITLE_PRIVACY_POLICY', 'Política de Privacidad');
+define('TITLE_STATION_LIST', 'Lista de Estaciones');
+define('TITLE_GITPULL', 'git pull');
+define('TITLE_STAFF', 'Gestión del Personal');
+define('TITLE_MAINTENANCE', 'Mantenimiento');
+define('TITLE_ALERT', 'Alertas');
+define('TITLE_PLANNING', 'Planificación');
+
+// DAYS & MONTHS
+define('MONDAY', 'Lunes');
+define('TUESDAY', 'Martes');
+define('WEDNESDAY', 'Miércoles');
+define('THURSDAY', 'Jueves');
+define('FRIDAY', 'Viernes');
+define('SATURDAY', 'Sábado');
+define('SUNDAY', 'Domingo');
+define('JANUARY', 'Enero');
+define('FEBRUARY', 'Febrero');
+define('MARCH', 'Marzo');
+define('APRIL', 'Abril');
+define('MAY', 'Mayo');
+define('JUNE', 'Junio');
+define('JULY', 'Julio');
+define('AUGUST', 'Agosto');
+define('SEPTEMBER', 'Septiembre');
+define('OCTOBER', 'Octubre');
+define('NOVEMBER', 'Noviembre');
+define('DECEMBER', 'Diciembre');
+
+// TRAIN PAGES
+define('TRAIN_ID', 'Identificador');
+define('TRAIN_TYPE', 'Tipo');
+define('TRAIN_CAPACITY', 'Capacidad');
+define('TRAIN_SPEED', 'Velocidad');
+define('TRAIN_LENGTH', 'Longitud');
+define('TRAIN_STATUS', 'Estado');
+define('TRAIN_FREE', 'Libre');
+define('TRAIN_USE', 'En uso');
+define('TRAIN_MAINTENANCE', 'En mantenimiento');
+define('ADD_TRAIN', 'Añadir un tren');
+
+// REGISTER PAGE
+define('PLEASE_ACCEPT', 'Por favor acepta ');
+
+// BOTONES
+define('BTN_RETURN', 'Volver');
+define('BTN_ADD', 'Añadir');
+define('BTN_DELETE', 'Eliminar');
+
+// TICKET
+define('YOUR_TRAVEL', 'Tu viaje');
+define('TICKETS_DETAIL', 'Detalles de tus boletos');
+define('NO_TICKET', '¡No tienes boleto por ahora!');
+define('NO_TICKET_RELATED', '¡No se encontró ningún boleto!');
+
+// BUY TICKET
+define('SELECT_SEATS', 'Selecciona tus asientos');
+
+// Home
+define('MY_ACCOUNT', 'Mi Cuenta');
+define('MY_TICKETS', 'Mis Boletos');
+define('MY_MESSAGES', 'Mis Mensajes');
+define('BUY_TICKET', 'Comprar un boleto');
+
+//FAQ
+define('HOW_TO_BUY', '¿Cómo comprar un billete?');
+define('HOW_TO_BUY_TEXT', 'Para comprar un billete, solo tienes que ir a la página de compra de billetes, seleccionar tu trayecto y validar tu compra. Luego recibirás un correo electrónico de confirmación con el detalle de tu billete.');
+define('HOW_TO_SEE_TICKET', '¿Cómo ver mis boletos?');
+define('HOW_TO_SEE_TICKET_TEXT', 'Para ver tus boletos, solo tienes que ir a la página "Mis boletos" después de iniciar sesión con tu dirección de correo electrónico y tu contraseña. Luego podrás ver tus boletos y descargarlos.');
+define('HOW_TO_CANCEL_TICKET', '¿Cómo cancelar un billete?');
+define('HOW_TO_CANCEL_TICKET_TEXT', 'Para cancelar un billete, solo tienes que ir a la página "Mis boletos" después de iniciar sesión con tu dirección de correo electrónico y tu contraseña. Luego podrás cancelar tu billete.');
+define('HOW_TO_SEE_MESSAGES', '¿Cómo ver mis mensajes?');
+define('HOW_TO_SEE_MESSAGES_TEXT', 'Para ver tus mensajes, solo tienes que ir a la página "Mis mensajes" después de iniciar sesión con tu dirección de correo electrónico y tu contraseña. Luego podrás ver tus mensajes.');
+define('HOW_TO_SEND_MESSAGES', '¿Cómo enviar una solicitud de asistencia?');
+define('HOW_TO_SEND_MESSAGES_TEXT', 'Para enviar una solicitud de asistencia, solo tienes que ir a la sección "Contáctanos" y enviar una solicitud de asistencia explicando el motivo de tu solicitud. Un miembro de nuestro equipo te responderá a la mayor brevedad posible.');
 
 // LOGIN & REGISTER
-define('LOGIN', 'Conexión');
-define('LOGOUT', 'Desconexión');
-define('REGISTER', 'Inscripción');
-define('TOREGISTER', "Inscribirse");
-define('INPUT_PASSWORD', 'Contraseña');
-define('INPUT_CONFIRMPASSWORD', 'Confirmar la contraseña');
-
+define('LOGIN', 'Iniciar sesión');
+define('LOGOUT', 'Cerrar sesión');
+define('REGISTER', 'Registrarse');
+define('TOREGISTER', "Registrarse");
+define('PASSWORD', 'Contraseña');
+define('CONFIRMPASSWORD', 'Confirmar contraseña');
 define('ERROR_NAME', 'El nombre no puede contener números');
-define('ERROR_FIRSTNAME', 'El apellido no puede contener números');
+define('ERROR_FIRSTNAME', 'El nombre no puede contener números');
 define('ERROR_MAIL', 'Correo electrónico no válido');
 define('ERROR_PHONE', 'Número de teléfono no válido');
-define('ERROR_PASSWORD_TOO_WEAK', 'La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y debe tener al menos 8 caracteres');
+define('ERROR_PASSWORD_TOO_WEAK', 'La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y tener al menos 8 caracteres');
 define('ERROR_PASSWORD_DIFFERENT', 'Las contraseñas son diferentes');
-define('ERROR_FORM', 'Corrija los errores antes de enviar sus datos');
+define('ERROR_FORM', 'Por favor corrija los errores antes de enviar sus datos');
+define('ERROR_CHARACTER', 'Número de caracteres insuficiente');
+define('DELETE_ACCOUNT', 'Eliminar mi cuenta');
+define('MAIL_ALREADY_USED', 'Esta dirección de correo electrónico ya está en uso');
 
-//contact
-define('INPUT_NAME', 'Nombre');
-define('INPUT_FIRST_NAME', 'Apellido');
-define('INPUT_PHONE', 'Teléfono');
-define('INPUT_EMAIL', 'Correo electrónico');
-define('INPUT_SUBJECT', 'Asunto');
-define('INPUT_DESC', 'Descripción');
-define('INPUT_SUBMIT', 'Enviar');
+//CONTACTO
+define('NAME', 'Nombre');
+define('FIRST_NAME', 'Apellido');
+define('PHONE', 'Número de teléfono');
+define('EMAIL', 'Correo electrónico');
+define('SUBJECT', 'Asunto');
+define('DESC', 'Descripción');
+define('SUBMIT', 'Enviar');
 
-// erreur
-define('TEXTE_PAGE_404', 'Oops, la página solicitada no existe!');
-define('MESSAGE_ERREUR', 'Se ha producido un error');
+//ERROR
+define('TEXTE_PAGE_404', '¡Oops, la página solicitada no existe!');
+define('MESSAGE_ERREUR', "Se ha producido un error");
 define('UNKWOWN_MAIL', 'Correo electrónico desconocido');
 define('UNKNOWN_PASSWORD', 'Contraseña incorrecta');
-define('ERREUR_QUERY', 'Problema de acceso a la base de datos. Contacte al administrador');
+define('ERREUR_QUERY', 'Problema al acceder a la base de datos. Póngase en contacto con el administrador');
+
+//LISTA DE ESTACIONES
+define('SEARCH', 'Buscar');
+
+//DETALLES DE LA ESTACIÓN
+define('STATION', 'Estación');
+define('CITY', 'Ciudad');
+define('REGION', 'Departamento');
+define('COUNTRY', 'País');
+define('STATION_OF', 'Estación de ');
+define('DEPARTURES', 'Salidas');
+define('ARRIVALS', 'Llegadas');
+define('TRAIN_NUMBER', 'Número de tren');
+define('DESTINATION', 'Destino');
+define('DEPARTURE_TIME', 'Hora de salida');
+define('ARRIVAL_TIME', 'Hora de llegada');
+define('NO_TRAIN', 'No hay trenes programados en este momento');
+define('NO_ARRIVAL', 'No hay llegadas programadas en este momento');
+define('NO_DEPARTURE', 'No hay salidas programadas en este momento');
+define('STATION_NOT_EXIST', 'Esta estación no existe');
+define('ON_TIME', 'A tiempo');
+define('DELAY_OF', 'Retraso de');
+define('MIN', 'min');
+define('FULL_SCREEN', 'Pantalla completa');
+
+//BOLETOS
+define('TICKET_DETAIL', 'Detalles de tu boleto');
+define('DURATION', 'Duración');
+define('TRAVEL', 'Viaje');
+define('TRAIN', 'Tren');
+define('BACK_TICKETS', 'Vuelve a tus boletos');
+define('MINUTES', 'minutos');
+define('TRAVEL_TO', 'Viaje hacia ');
+define('BUY', 'Comprar');
+
+//INICIO CLIENTE
+define('WELCOME_PT1', 'Bienvenido a ');
+define('WELCOME_PT2', ', ¡tu sitio de reserva de boletos de tren en línea!');
+define('WELCOME_TEXT', "Estamos contentos de recibirte en nuestra plataforma y ofrecerte la oportunidad de reservar en pocos clics boletos de tren para tu próximo viaje. Ya sea que desees viajar en Francia o en Europa, tenemos lo que necesitas.");
+define('WELCOME_TEXT2', "Esperamos que tu viaje en tren sea agradable y que disfrutes de nuestro servicio de reserva en línea. ¡Buen viaje con Tchoutchou!");
+define('SEARCH_TEXT', 'Nuestro sitio es simple y fácil de usar, podrás encontrar y reservar tu boleto de tren en pocos minutos. También ofrecemos muchas opciones para personalizar tu viaje, como la posibilidad de elegir tu asiento.');
+define('STATION_TEXT', "Estamos contentos de anunciar que ahora puedes acceder a la lista completa de estaciones de nuestra red. Así puedes ver todas las estaciones que servimos con nuestros trenes, así como sus coordenadas y horarios de salida y llegada de los trenes.");
+define('MESSAGE_TEXT', "También puedes contactarnos directamente desde nuestro sitio, para hacernos saber tus comentarios o sugerencias. Estamos a tu disposición y te responderemos lo antes posible.");
+define('PLANNING_TEXT', "También puedes consultar el horario de tus trayectos, para saber cuándo salen y cuándo llegan. Así puedes preparar tu viaje con anticipación y asegurarte de que estarás a tiempo para tu tren.");
+define('ALERT_LIST', 'Lista de alertas');
+define('ALERT_LIST_TEXT', 'Puedes consultar la lista de alertas para saber su origen, su ubicación y su impacto.');
+
+// HOME EMPLEADO
+define('WELCOME', 'Bienvenido ');
+define('WELCOME_TEXT_EMP', "Actualmente estás conectado como empleado, puedes acceder a tu espacio de trabajo haciendo clic en los botones a continuación.");
+define('PLATFORM_MANAGER', 'Gestión de andenes');
+define('PLATFORM_MANAGER_TEXT', 'Acceder al servicio de gestión de andenes');
+define('WELCOME_GO', 'acceder');
+define('MAINTENANCE', 'Mantenimiento');
+define('MAINTENANCE_TEXT', 'Acceder al servicio de mantenimiento');
+
+// GESTOR DE ANDENES
+define('SELECT_STATION', 'Seleccionar estación');
+define('SELECT_HUB', 'Seleccionar centro de distribución');
+define('CONFIG_PANNEL', 'Panel de configuración');
+define('PLATFORM_MANAGER_TITLE', 'Gestor de andenes');
+define('APPROACHING_TRAIN', 'Tren en aproximación');
+define('PLATFORMS', 'Andenes');
+define('LETTER', 'Letra');
+define('CLOSE', 'Cerrado');
+define('OPEN', 'Abierto');
+define('CONFIRM', 'Confirmar');
+
+// MENSAJES
+define('NO_MESSAGES', '¡Nunca has intentado contactar nuestro soporte hasta ahora!');
+define('NO_MESSAGE_BTN', 'Acceder al formulario de contacto');
+define('CHOOSE_MESSAGE', 'Debes seleccionar una discusión a la izquierda de tu pantalla para cargar los mensajes');
+define('INTERN', 'Interno');
+define('EXTERN', 'Externo');
+define('YOUR_MESSAGE', "Tu mensaje");
+define('MESSAGES', 'Mensajes');
+define('SEND', 'Enviar');
+define('STORAGE', 'Archivo');
+define('MESSAGE_TOO_LONG', 'Tu mensaje es demasiado largo (no debe exceder 1000 caracteres)');
+
+// ALERTAS
+define('LOCATION', 'Ubicación');
+define('DESCRIPTION', 'Descripción');
+define('SEND_ALERT', 'Enviar una alerta');
+define('SELECT_ALERT_TYPE', 'Debes seleccionar un tipo de alerta');
+define('ALERT_CREATED', 'La alerta se ha creado correctamente');
+define('AUTHORIZE_LOCATION', 'Debes autorizar la geolocalización para enviar una alerta');
+define('ALERT_TYPE', 'Tipo');
+define('TRAVEL_ID', 'ID del viaje');
+define('ALERT_MESSAGE', 'Mensaje');
+define('ALERT_DATE', 'Fecha de la alerta');
+define('NO_ALERTS', 'No hay alertas por el momento');
+define('ALL_TYPES_ALERTS', 'todos los tipos');
+define('ALL_STATUS_TRAVELS', 'Todos los estados');
+define('ALERT_STATUS', 'Estado');
+define('FINISHED_ALERTS', 'Terminadas');
+define('CURRENT_ALERTS', 'En curso');
+define('RESET_FILTER', 'Reiniciar todos los filtros');
+define('ALERTS_HEADER', 'Alerta sobre el viaje n° ');
+define('ALERT_DOES_NOT_EXIST', 'Esta alerta no existe');
+define('ALERT_NOT_FINISHED', 'Marcar como terminada');
+define('ALERT_FINISHED', 'La alerta ya está terminada');
+define('SHOW_ALL_ALERTS', 'Mostrar también las alertas terminadas');
+
+// COMPRA
+define('NO_TRAVEL_FOUND', 'No hay viajes que coincidan con tu búsqueda');
+define('SEARCH_TRAVEL', 'Buscar un viaje');
+define('PREVIOUS', 'Anterior');
+define('NEXT', 'Siguiente');
+define("NO_SELECTED_SEAT", "Ningún asiento ha sido seleccionado");
+define('SELECTED_SEAT', 'Has seleccionado los siguientes asientos: ');
+define('VALIDATE', 'Validar mi asiento');
+define('SEAT_NEEDED', 'Debes elegir el número de asientos que has reservado para poder validar tu asiento');
+define('NO_SEAT_NUMBER', 'Asiento libre n°');
+define('SEAT_NUMBER', 'Asiento n°');
+define('END_STATION', 'Llegada');
+define('START_STATION', 'Salida');
+define('ERROR_STATIONS', 'Las estaciones de salida y llegada no pueden ser iguales');
+define('ERROR_DATE', 'La fecha de salida no puede ser anterior a la fecha de hoy');
+define('EMPTY_INPUT_BUY', 'Debes llenar todos los campos');
+define('WRONG_STATIONS', 'Por favor, elige estaciones existentes');
+define('ERROR_NAMES', 'No puedes tener el mismo nombre y apellido');
+define('ERROR_NAMES_EMPTY', 'Debes llenar todos los campos de nombre y apellido');
+define('GROUND_LEVEL',"Planta baja");
+define('FIRST_LEVEL',"Primer piso");
+
+// ORDENACIÓN
+define('SORT_STATION', 'Ordenar por Estación');
+define('SORT_CITY', 'Ordenar por Ciudad');
+define('SORT_REGION', 'Ordenar por Departamento');
+define('SORT_COUNTRY', 'Ordenar por País');
+
+// PERSONAL
+define('DEPARTMENT', 'Servicio');
+define('ADD_STAFF', 'Añadir un empleado');
+define('IDENTITY', 'Identidad');
+define('TYPE', 'Tipo');
+define('ADMINISTRATOR', 'Administrador');
+define('DRIVER', 'Conductor');
+define('SERVICE', 'Servicio');
+define('SUPPORT', 'Soporte');
+
+// ALT DE IMÁGENES
+define('ALT_WOMEN_WITH_LUGGAGE', 'Mujer con equipaje');
+define('ALT_PHONE_SEARCH', 'Búsqueda por teléfono');
+define('ALT_WOMEN_SEARCH','Mujer realizando una búsqueda');
+define('ALT_ARROW_RIGHT', 'Flecha hacia la derecha');
