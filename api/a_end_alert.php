ec8bcf84fb2319988c1445855644ce00bb7ef183
Qypol342 <Qypol342@gmail.com>
2023-01-08 21:41:30 +0100

    refactor(end-alert): change to new standar

---
+++ api/a_end_alert.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    
+    
+    require_once(PATH_MODELS . 'AlertDAO.php');
+
+    //ici tu met le php que tu veux avec le dao et la requete dessus qui va s'executer
+    $alert = new AlertDAO();
+
+    $id_alert = $_GET['id'];
+
+    $alert->updateAlertStatus(substr($_GET['id'], 0, sizeof($_GET['id']) - 6), substr($_GET['id'], -6, 2), substr($_GET['id'], -4, 2), substr($_GET['id'], -2, 2), 0);
