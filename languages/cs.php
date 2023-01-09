73f0c561de50f00360bf18e69bb18d635685de31
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-01-09 19:10:53 +0100

    Fix languages

--- languages/cs.php
+++ languages/cs.php
@@ -1,50 +1,96 @@
 <?php
-
-define('LOGO', 'Logo společnosti'); // Zobrazeno, pokud není obrázek nalezen
-define('MENU_ACCUEIL', 'Domů');
-define('TITRE_PAGE_ACCUEIL', 'Domovská stránka');
-
-//HEADER & FOOTER
+//TITLE
 define('TITLE', 'Tchoutchou');
 define('TITLE_HOME', 'Domů');
-define('TITLE_PURCHASE', 'Nákup');
-define('TITLE_SEARCH', 'Vyhledávání');
-define('TITLE_TICKET', 'Vstupenky');
-define('TITLE_LOGIN', 'Přihlášení');
+define('TITLE_PURCHASE', 'Nákupy');
+define('TITLE_SEARCH', 'Hledat');
 define('TITLE_INFORMATION', 'Informace');
 define('TITLE_CONTACT', 'Kontaktujte nás');
 define('TITLE_COMPANY', 'Naše společnost');
-define('TITLE_INFORM_YOU', 'Informační sekce');
+define('TITLE_INFORM_YOU', 'Informovat vás');
 define('TITLE_QUICK_ACCESS', 'Rychlý přístup');
-define('STATION_INFORMATION', 'Informace o stanici a platformě');
-define('TERMS_OF_USE', 'Podmínky použití');
-define('PRIVACY_POLICY', 'Zásady ochrany osobních údajů');
+define('TITLE_PLATFORM_MANAGER', 'Správa nástupišť');
+define('TITLE_TERMS_OF_USE', 'Všeobecné podmínky');
+define('TITLE_PRIVACY_POLICY', 'Zásady ochrany osobních údajů');
+define('TITLE_STATION_LIST', 'Seznam nádraží');
+define('TITLE_GITPULL', 'git pull');
+define('TITLE_STAFF', 'Správa personálu');
+define('TITLE_MAINTENANCE', 'Údržba');
+define('TITLE_ALERT', 'Varování');
+define('TITLE_PLANNING', 'Plánování');
+
+// DAYS & MONTHS
+define('MONDAY', 'Pondělí');
+define('TUESDAY', 'Úterý');
+define('WEDNESDAY', 'Středa');
+define('THURSDAY', 'Čtvrtek');
+define('FRIDAY', 'Pátek');
+define('SATURDAY', 'Sobota');
+define('SUNDAY', 'Neděle');
+define('JANUARY', 'Leden');
+define('FEBRUARY', 'Únor');
+define('MARCH', 'Březen');
+define('APRIL', 'Duben');
+define('MAY', 'Květen');
+define('JUNE', 'Červen');
+define('JULY', 'Červenec');
+define('AUGUST', 'Srpen');
+define('SEPTEMBER', 'Září');
+define('OCTOBER', 'Říjen');
+define('NOVEMBER', 'Listopad');
+define('DECEMBER', 'Prosinec');
+
+//TRAIN PAGES
+define('TRAIN_ID', 'Identifikátor');
+define('TRAIN_TYPE', 'Typ');
+define('TRAIN_CAPACITY', 'Kapacita');
+define('TRAIN_SPEED', 'Rychlost');
+define('TRAIN_LENGTH', 'Délka');
+define('TRAIN_STATUS', 'Stav');
+define('TRAIN_FREE', 'Volný');
+define('TRAIN_USE', 'V použití');
+define('TRAIN_MAINTENANCE', 'V údržbě');
+define('ADD_TRAIN', 'Přidat vlak');
+
+//REGISTER PAGE
+define('PLEASE_ACCEPT', 'Prosím, přijměte ');
+
+//BUTTONS
+define('BTN_RETURN', 'Zpět');
+define('BTN_ADD', 'Přidat');
+define('BTN_DELETE', 'Odstranit');
+
+//TICKET
+define('YOUR_TRAVEL', 'Váš cestování');
+define('TICKETS_DETAIL', 'Podrobnosti o vašich jízdenkách');
+define('NO_TICKET', 'Nemáte v tuto chvíli žádnou jízdenku!');
+define('NO_TICKET_RELATED', 'Žádná jízdenka neodpovídá!');
+
+//BUY TICKET
+define('SELECT_SEATS', 'Vyberte si sedadla');
+
+//Home
+define('MY_ACCOUNT', 'Můj účet');
+define('MY_TICKETS', 'Moje jízdenky');
+define('MY_MESSAGES', 'Moje zprávy');
+define('BUY_TICKET', 'Koupit jízdenku');
+
+//FAQ
+define('HOW_TO_BUY', 'Jak koupit jízdenku?');
+define('HOW_TO_BUY_TEXT', 'Pro nákup jízdenky navštivte stránku pro nákup jízdenek, vyberte si svou cestu a potvrďte nákup. Poté obdržíte potvrzovací e-mail s podrobnostmi o vaší jízdence.');
+define('HOW_TO_SEE_TICKET', 'Jak zobrazit moje jízdenky?');
+define('HOW_TO_SEE_TICKET_TEXT', 'Pro zobrazení jízdenek se přihlaste pomocí své e-mailové adresy a hesla a přejděte na stránku "Moje jízdenky". Zobrazí se vám vaše jízdenky a budete je moci stáhnout.');
+define('HOW_TO_CANCEL_TICKET', 'Jak zrušit jízdenku?');
+define('HOW_TO_CANCEL_TICKET_TEXT', 'Pro zrušení jízdenky se přihlaste pomocí své e-mailové adresy a hesla a přejděte na stránku "Moje jízdenky". Poté budete moci zrušit svoji jízdenku.');
+define('HOW_TO_SEE_MESSAGES', 'Jak zobrazit mé zprávy?');
+define('HOW_TO_SEE_MESSAGES_TEXT', 'Pro zobrazení vašich zpráv se přihlaste pomocí své e-mailové adresy a hesla a přejděte na stránku "Moje zprávy". Zobrazí se vám vaše zprávy.');
+define('HOW_TO_SEND_MESSAGES', 'Jak odeslat žádost o podporu?');
+define('HOW_TO_SEND_MESSAGES_TEXT', 'Pro odeslání žádosti o podporu navštivte sekci "Kontaktujte nás" a požádejte o podporu s vysvětlením důvodu vaší žádosti. Člen našeho týmu vám odpoví co nejdříve.');
 
 // LOGIN & REGISTER
-
-define('ERROR_NAME', "Vaše jméno nemůže obsahovat číslice");
-define('ERROR_FIRSTNAME', "Vaše křestní jméno nemůže obsahovat číslice");
 define('LOGIN', 'Přihlášení');
 define('LOGOUT', 'Odhlášení');
-define('REGISTER', 'Registrovat');
-define('TOREGISTER', "Registrovat");
-define('INPUT_PASSWORD', 'Heslo');
-define('INPUT_CONFIRMPASSWORD', 'Potvrzení hesla');
-
-//contact
-define('INPUT_NAME', 'Jméno');
-define('INPUT_FIRST_NAME', 'Křestní jméno');
-define('INPUT_PHONE', 'Telefon');
-define('INPUT_EMAIL', 'Email');
-define('INPUT_SUBJECT', 'Předmět');
-define('INPUT_DESC', 'Popis');
-define('INPUT_SUBMIT', 'Odeslat');
-
-// erreur
-define('ERROR_PHONE', 'Neplatné telefonní číslo');
-define('ERROR_MAIL', 'Neplatný email');
-define('TEXTE_PAGE_404', 'Oups, požadovaná stránka neexistuje!');
-define('MESSAGE_ERREUR', "Došlo k chybě");
-define('UNKWOWN_MAIL', 'Neznámý mail');
-define('UNKNOWN_PASSWORD', 'Nesprávné heslo');
-define('ERREUR_QUERY', 'Problém přístupu do databáze. Kontaktujte správce');
+define('REGISTER', 'Registrace');
+define('TOREGISTER', "Zaregistrovat se");
+define('PASSWORD', 'Heslo');
+define('CONFIRMPASSWORD', 'Potvrzení hesla');
