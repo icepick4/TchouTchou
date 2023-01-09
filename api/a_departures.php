993449569d34d21d41f3efcfec07d17fb75fc0ea
Larcherbc <jules.rbc@gmail.com>
2023-01-09 07:52:45 +0100

    add ajax to station_detail to refresh the screen

---
+++ api/a_departures.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+
+
+    require_once(PATH_MODELS . 'StationDAO.php');
+    $train = new StationDAO();
+    $departTravels = $train->get_station_departure($_GET['id']); ?>
+
+                <table>
+                    <tbody>
+                        <?php
+                        foreach ($departTravels as $travel) {
+                        ?>
+                            <tr>
+                                <td><?= SVG_LOGO ?></td>
+                                <td>
+                                    <p><? if ($travel['LATE_TIME'] == null) {
+                                            echo ON_TIME;
+                                        } else {
+                                            echo DELAY_OF . $travel['LATE_TIME'] . MIN;
+                                        }  ?></p>
+                                    <p>n°<?= $travel['TRAIN_ID'] ?></p>
+                                </td>
+                                <td>
+                                    <h4><?= strTo24Time($travel['DEPARTURE_TIME']) ?></h4>
+                                </td>
+                                <td>
+                                    <h3><?= $travel['DESTINATION'] ?></h3>
+                                    <p><? if ($travel['LATE_TIME'] == null) {
+                                            echo ON_TIME;
+                                        } else {
+                                            echo DELAY_OF . $travel['LATE_TIME'] . MIN;
+                                        }  ?></p>
+                                </td>
+                                <td><? $platform = $train->get_platform_used_for_travel($travel['TRAVEL_ID'], $_GET['id']);
+                                    if ($platform['PLATFORM_LETTER'] != null)
+                                        echo "<p>" . $platform['PLATFORM_LETTER'] . "</p>"; ?></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
