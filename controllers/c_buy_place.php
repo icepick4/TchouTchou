2810a943de94cf9317484e5df039aca53b98e5cf
Larcherbc <jules.rbc@gmail.com>
2023-01-09 08:43:55 +0100

    fix access for account

--- controllers/c_buy_place.php
+++ controllers/c_buy_place.php
@@ -1,5 +1,15 @@
 <?php
 
+if($_SESSION['logged'] == false){
+    header('Location: index.php?page=login');
+    exit();
+}
+
+if(!isset($_POST['price']) || !isset($_POST['nbr']) || !isset($_POST['travel']) || !isset($_POST['line']) || !isset($_POST['from']) || !isset($_POST['to']) || !isset($_POST['name_1']) || !isset($_POST['firstname_1'])){
+    header('Location: index.php?page=buy');
+    exit();
+}
+
 require_once(PATH_MODELS . 'TrainDAO.php');
 require_once(PATH_MODELS . 'TravelDAO.php');
 $train = new TrainDAO();
