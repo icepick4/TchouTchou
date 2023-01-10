07501fd937ba367668774d2473246a3207444603
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-01-10 09:19:56 +0100

    fix(Alert_list): Fixing parenthesis

--- views/v_alert_list.php
+++ views/v_alert_list.php
@@ -18,13 +18,13 @@
                     <option value="<?= $alertType['ALERT_TYPE_LABEL'] ?>"><?= $alertType['ALERT_TYPE_LABEL'] ?></option>
                 <?php } ?>
             </select></div>
-        <? if (isset($_GET['showAll'])) { ?>
+        <?php if (isset($_GET['showAll'])) { ?>
             <div class="selectArrow"><select id="selectStatus">
                     <option value="all"><?= ALL_STATUS_TRAVELS ?></option>
                     <option value="<?= FINISHED_ALERTS ?>"><?= FINISHED_ALERTS ?></option>
                     <option value="<?= CURRENT_ALERTS ?>"><?= CURRENT_ALERTS ?></option>
                 </select></div>
-        <? } ?>
+        <?php } ?>
 
         <button id="resetFilter"><?= RESET_FILTER ?></button>
     </div>
@@ -35,15 +35,16 @@
                 <th><?= TRAVEL_ID ?></th>
                 <th><?= ALERT_MESSAGE ?></th>
                 <th><?= ALERT_DATE ?></th>
-                <? if (isset($_GET['showAll'])) { ?>
+                <?php if (isset($_GET['showAll'])) { ?>
                     <th><?= ALERT_STATUS ?></th>
-                <? } ?>
+                <?php } ?>
             </tr>
         </thead>
         <tbody>
 
 <?php
 foreach ($alertList as $alert) {
+    
     ?>
         <?php 
             if ($alert['ALERT_TYPE_ID'] <10)
@@ -55,17 +56,13 @@ foreach ($alertList as $alert) {
                 <tr class="alerts" onclick="document.location.href='<?='index.php?page=alert_single&id='.$alert['TRAVEL_ID'].$alert['ALERT_TYPE_ID'].substr($alert['DATETIME_TRAVEL'],-5,2).substr($alert['DATETIME_TRAVEL'],-2,2)."'"?>">
             <?php
             foreach ($alertList as $alert) {
-            ?>
-                <?php
                 if ($alert['ALERT_TYPE_ID'] < 10) {
-
-                ?><script>
-                        console.log("<?= 'index.php?page=alert_single&id=' . $alert['TRAVEL_ID'] . '0' . $alert['ALERT_TYPE_ID'] . substr($alert['DATETIME_TRAVEL'], -5, 2) . substr($alert['DATETIME_TRAVEL'], -2, 2) ?>");
-                    </script>
+                ?>
                     <tr class="alerts" onclick="document.location.href=<?= 'index.php?page=alert_single&id=' . $alert['TRAVEL_ID'] . '0' . $alert['ALERT_TYPE_ID'] . substr($alert['DATETIME_TRAVEL'], -5, 2) . substr($alert['DATETIME_TRAVEL'], -2, 2) ?>">
                     <?php } else { ?>
                     <tr class="alerts" onclick="document.location.href=<?= 'index.php?page=alert_single&id=' . $alert['TRAVEL_ID'] . $alert['ALERT_TYPE_ID'] . substr($alert['DATETIME_TRAVEL'], -5, 2) . substr($alert['DATETIME_TRAVEL'], -2, 2) ?>">
                     <?php
+                    
                 }
                     ?>
                     <td><?= $alert['ALERT_TYPE_LABEL'] ?></td>
@@ -73,12 +70,12 @@ foreach ($alertList as $alert) {
                     <td><?= $alert['ALERT_MESSAGE'] ?></td>
                     <td><?= $alert['DATETIME_TRAVEL'] ?></td>
                     <?php if (isset($_GET['showAll'])) { ?>
-                        <td><? if ($alert['ALERT_STATUS'] == 0) {
+                        <td><?php if ($alert['ALERT_STATUS'] == 0) {
                                 echo FINISHED_ALERTS;
                             } else {
                                 echo CURRENT_ALERTS;
                             } ?></td>
-                    <? } ?>
+                    <?php } ?>
                     </tr>
 
                 <?php
@@ -88,8 +85,8 @@ foreach ($alertList as $alert) {
         </tbody>
     </table>
 
-
-<?php } ?>
+        
+<?php } }?>
 
 <?php if (!isset($_GET['showAll'])) { ?>
     <a id="buttonAllAlert" href="index.php?page=alert_list&showAll=true"><?= SHOW_ALL_ALERTS ?></a>
