043938168e27cad690ee302979df8d90787d9aee
icepick4 <remi.jara@laposte.net>
2023-01-11 15:51:26 +0100

    little changes about echos

--- web/controllers/c_station_detail.php
+++ web/controllers/c_station_detail.php
@@ -6,14 +6,12 @@ require_once(PATH_MODELS . 'TravelDAO.php');
 $train = new StationDAO();
 $travel = new TravelDAO();
 
-if(!isset($_GET['id']) || empty($_GET['id'])){
+if (!isset($_GET['id']) || empty($_GET['id'])) {
     header('Location: index.php');
     exit();
 }
 
 $station_name = $train->get_station_name($_GET['id']);
 $departTravels = $train->get_station_departure($_GET['id']);
-print_r($departTravels);
 $arrivalTravels = $train->get_station_arrivals($_GET['id']);
-print_r($arrivalTravels);
 require_once(PATH_VIEWS . $page . '.php');
