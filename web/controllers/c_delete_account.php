31b9c3aae0079e905976e46ba8edaeb61dfc074b
Niskko <niskko.yt@gmail.com>
2023-01-11 17:40:14 +0100

    début restriction employee suprression

--- web/controllers/c_delete_account.php
+++ web/controllers/c_delete_account.php
@@ -3,9 +3,11 @@
 require_once(PATH_MODELS . 'UserDAO.php');
 $user = new UserDAO();
 if (isset($_GET['verif'])) {
-    if (($_GET['verif'] == 214428544845492) && isset($_SESSION['user_id'])) {
-        $user->deleteUser($_SESSION['user_id']);
-        $_SESSION = array();
+    if ($result['USER_CATEGORIE_ID'] == 0) {
+        if (($_GET['verif'] == 214428544845492) && isset($_SESSION['user_id'])) {
+            $user->deleteUser($_SESSION['user_id']);
+            $_SESSION = array();
+        }
+        header("Location: index.php?page=home");
     }
 }
-header("Location: index.php?page=home");
