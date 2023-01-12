278a165f38d8cdda8e20c7347f32b202d3c9286b
Larcherbc <jules.rbc@gmail.com>
2023-01-12 19:19:45 +0100

    add searchbar in travel

--- web/controllers/c_travel.php
+++ web/controllers/c_travel.php
@@ -1,8 +1,12 @@
 <?php
 require_once(PATH_MODELS . 'StaffDAO.php');
+require_once(PATH_MODELS . 'StationDAO.php');
+
 $staff = new StaffDAO();
+$station = new StationDAO();
 
 if ($staff->isAdministrator($_SESSION['user_id'])) {
+    $stations = $station->get_stations();
     require_once(PATH_VIEWS . $page . '.php');
 } else {
     header("Location: index.php?page=home");
