d4bc75e9b6e2018de92152b78af05555df814a1a
icepick4 <remi.jara@laposte.net>
2023-01-19 16:34:48 +0100

    adding data user in pdf

--- web/pdf.php
+++ web/pdf.php
@@ -13,13 +13,20 @@ $user = new UserDAO();
 $discussions = new MessageDAO();
 $pdf->SetFont('Arial', '', 18);
 $allDiscussions = $discussions->getUserDiscussions($_SESSION['user_id']);
+$userData = $user->getUserById($_SESSION['user_id']);
 $i = 0;
+$pdf->Cell(0,20, 'INFORMATIONS PERSONNELLES', 0, 2);
+$pdf->Cell(0,10, 'Prenom' . ' : ' . $userData['USER_FIRSTNAME'], 0, 2);
+$pdf->Cell(0,10, 'Nom' . ' : ' . $userData['USER_LASTNAME'], 0, 2);
+$pdf->Cell(0,10, 'Email' . ' : ' . $userData['USER_MAIL'], 0, 2);
+$pdf->Cell(0,10, 'Telephone' . ' : ' . $userData['USER_PHONE'], 0, 2);
+$pdf->AddPage();
 foreach ($allDiscussions as $discussion) {
     $currentDiscussion = $discussions->getDiscussionById($discussion['DISCUSSION_ID']);
     $pdf->Cell(0, 10, SUBJECT . ' : ' . $discussion['DISCUSSION_SUBJECT'], 0, 2);
     $pdf->Cell(0, 10, DATE . ' : ' . $discussion['CREATION_TIME'], 0, 2);
     foreach ($currentDiscussion as $message) {
-        $text = $user->getUserById($message['SENDER_ID'])['USER_FIRSTNAME'] . ' : ' . $message['MESSAGE_CONTENT'];
+        $text = $userData['USER_FIRSTNAME'] . ' : ' . $message['MESSAGE_CONTENT'];
         $pdf->MultiCell(0, 10, $text, 0, 2);
     }
     if ($i < count($allDiscussions) - 1){
